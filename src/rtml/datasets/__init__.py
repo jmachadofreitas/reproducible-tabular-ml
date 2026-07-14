da97111bdfba91c1831a1c7b69bdbdc658745c0d
João Machado de Freitas <jmachadofreitas@vivaldi.net>
2026-07-14 13:28:41 +0200

    refactor: add package exports

---
+++ src/rtml/datasets/__init__.py
@@ -0,0 +1,9 @@
+from rtml.datasets.data import Dataset, FeatureInfo, FeatureKind, FeatureSchema, FeatureTag
+
+__all__ = [
+    "Dataset",
+    "FeatureInfo",
+    "FeatureKind",
+    "FeatureSchema",
+    "FeatureTag",
+]
